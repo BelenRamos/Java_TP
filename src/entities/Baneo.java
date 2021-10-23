bd43f63ca445d675cbf7e3a1c5f73cc6714632e3
belen <belen@DESKTOP-31BLD0N>
2021-10-23 17:02:59 -0300

    primer commit

--- src/entities/Baneo.java
+++ src/entities/Baneo.java
@@ -2,13 +2,44 @@ package entities;
 
 import java.sql.Date;
 
-public class Baneo {
+public class Baneo extends Usuario  {
 	
 	int id_baneo; 
 	String motivo; 
 	Date fecha_hasta; 
 	Date fecha_desde; 
-	String descargo; 
-	int id_usuario; //Moderador
+	String descargo;
+	public int getId_baneo() {
+		return id_baneo;
+	}
+	public void setId_baneo(int id_baneo) {
+		this.id_baneo = id_baneo;
+	}
+	public String getMotivo() {
+		return motivo;
+	}
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+	public Date getFecha_hasta() {
+		return fecha_hasta;
+	}
+	public void setFecha_hasta(Date fecha_hasta) {
+		this.fecha_hasta = fecha_hasta;
+	}
+	public Date getFecha_desde() {
+		return fecha_desde;
+	}
+	public void setFecha_desde(Date fecha_desde) {
+		this.fecha_desde = fecha_desde;
+	}
+	public String getDescargo() {
+		return descargo;
+	}
+	public void setDescargo(String descargo) {
+		this.descargo = descargo;
+	} 
+	
+	
 
 }
